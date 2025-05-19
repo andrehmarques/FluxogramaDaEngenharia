72073a003aeccf34272f8f81e764314cc75f997b
Andreh Marques <andrehmarquez@gmail.com>
2025-05-18 22:41:22 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿string escolhaUsuario;
+
+do
+{
+    Console.WriteLine("O objeto está se movendo? Sim ou Não: ");
+    escolhaUsuario = Console.ReadLine()!.ToUpper();
+
+    if (escolhaUsuario == "S" || escolhaUsuario == "N")
+    {
+        break;
+    }
+
+    else
+    {
+        Console.WriteLine("Resposta inválida. Digite S para sim ou N para não.");
+    }
+} while (true);
+
+if (escolhaUsuario == "S")
+{
+    do
+    {
+        Console.WriteLine("O objeto deveria se mover? Sim ou Não: ");
+        escolhaUsuario = Console.ReadLine()!.ToUpper();
+
+        if (escolhaUsuario == "S" || escolhaUsuario == "N")
+        {
+            break;
+        }
+
+        else
+        {
+            Console.WriteLine("Resposta inválida. Digite S para sim ou N para não.");
+        }
+
+    } while (true);
+
+    if (escolhaUsuario == "S")
+    {
+        Console.WriteLine("Ótimo!");
+
+    }
+
+    else
+    {
+        Console.WriteLine("Use Silver Tape!");
+    }
+}
+
+else
+{
+    do
+    {
+        Console.WriteLine("O objeto deveria se mover? Sim ou Não: ");
+        escolhaUsuario = Console.ReadLine()!.ToUpper();
+
+        if (escolhaUsuario == "S" || escolhaUsuario == "N")
+        {
+            break;
+        }
+
+        else
+        {
+            Console.WriteLine("Resposta inválida. Digite S para sim ou N para não.");
+        }
+
+    } while (true);
+
+    if (escolhaUsuario == "N")
+    {
+        Console.WriteLine("Ótimo!");
+    }
+
+    else
+    {
+        Console.WriteLine("Use WD-40!");
+    }
+
+}
